e24f5327cfe2c1fa8dc5c1ee568de4ad6e22a379
yurley <yurs.ksf1@gmail.com>
2023-06-02 21:04:29 -0500

    definicion factorial

--- factorial.js
+++ factorial.js
@@ -5,12 +5,41 @@
 // N! = (N) * (N-1) * (N-2) .... * (2) * (1)
 
 
-// Usando un For 
-numero_factorial = 11
-total_factorial = 1
+// Usando un For
+function calcularFactorial(numero_factorial) {
+    total_factorial = 1
 
-for (let index = 1; index <= numero_factorial; index++) {
-    total_factorial = total_factorial * index
+    for (let index = 1; index <= numero_factorial; index++) {
+        total_factorial = total_factorial * index
+    }
+
+    return total_factorial
 }
 
+numero_factorial = 4
+total_factorial = calcularFactorial(numero_factorial)
 console.log(`el factorial de ${numero_factorial} es ${total_factorial}`)
+
+
+numero_factorial = 5
+total_factorial = calcularFactorial(numero_factorial)
+console.log(`el factorial de ${numero_factorial} es ${total_factorial}`)
+
+numero_factorial = 8
+total_factorial = calcularFactorial(numero_factorial)
+console.log(`el factorial de ${numero_factorial} es ${total_factorial}`)
+
+
+// Usando programacion recursiva
+function calcularFactorialRecursivamente(numero_factorial) {
+    if (numero_factorial == 1) {
+        return numero_factorial
+    }
+
+    return numero_factorial * calcularFactorialRecursivamente(numero_factorial - 1)
+}
+
+numero_factorial = 8
+total_factorial = calcularFactorialRecursivamente(numero_factorial)
+console.log(`el factorial de ${numero_factorial} es ${total_factorial}`)
+
